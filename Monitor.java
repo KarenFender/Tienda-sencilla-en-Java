4993a0055274305394590342c48f2a33170b3be6
Karen <karenkobos@hotmail.com>
2014-11-11 19:25:14 -0600

    Create Monitor.java
    Básicamente todas estas clases van a ser iguales, sólo que con atributos particulares a parte de los que se heredan la clase Artículos.

---
+++ Monitor.java
@@ -0,0 +1,13 @@
+package Articles;
+
+public class Monitor extends Articulos {
+	public int resolucion;
+	
+	public Monitor(int a, int b, int c, String d,boolean e)
+	{
+		super(b,c,d,e);
+		resolucion = a;
+		
+		
+	}
+}
