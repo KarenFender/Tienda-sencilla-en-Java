3a9e9f0d723e7b1d0c9b4f3dbd57e0338786aca9
Karen <karenkobos@hotmail.com>
2014-11-11 19:48:29 -0600

    Create Bocinas.java

---
+++ Bocinas.java
@@ -0,0 +1,12 @@
+package Articles;
+
+public class Bocinas extends Articulos {
+	public int maxVolumen;
+	
+	public Bocinas(int a, int b, int d, String e,boolean f)
+	{
+		super(b,d,e,f);
+		maxVolumen = a;
+	}
+}
+
