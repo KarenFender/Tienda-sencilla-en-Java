e3e4cb11aa7b99d8b042a6608d6c171499655fca
Karen <karenkobos@hotmail.com>
2014-11-11 19:48:10 -0600

    Create Teclado.java

---
+++ Teclado.java
@@ -0,0 +1,12 @@
+package Articles;
+
+public class Teclado extends Articulos {
+	public int numTeclas;
+	
+	public Teclado(int a, int b, int d, String e,boolean f)
+	{
+		super(b,d,e,f);
+		numTeclas = a;
+	}
+
+}
